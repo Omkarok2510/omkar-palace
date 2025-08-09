fbc129368ae9d9c4465493f48ae857b7a106ac43
Omkarok2510 <87378419+Omkarok2510@users.noreply.github.com>
2025-08-09 14:31:53 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,21 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
+import('next').NextConfig};
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
+module.exports = nextConfig;
